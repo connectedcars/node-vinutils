dc3e46af905878de64f11a8991e876a0cfaf9d14
Michael Storgaard <git@mex.io>
2017-11-17 14:42:53 +0100

    Added files

---
+++ src/index.js
@@ -0,0 +1,483 @@
+const Make = {
+  AUDI: 'Audi',
+  SEAT: 'SEAT',
+  SKODA: 'Škoda',
+  VOLKSWAGEN: 'Volkswagen'
+}
+
+const Model = {
+  [Make.AUDI]: {
+    A1: 'A1',
+    A3: 'A3',
+    A4: 'A4',
+    A5: 'A5',
+    A6: 'A6',
+    A7: 'A7',
+    A8: 'A8',
+    Q2: 'Q2',
+    Q3: 'Q3',
+    Q5: 'Q5',
+    Q7: 'Q7',
+    R8: 'R8',
+    RS3: 'RS3',
+    RS4: 'RS4',
+    RS5: 'RS5',
+    RS6: 'RS6',
+    RS7: 'RS7',
+    S1: 'S1',
+    S3: 'S3',
+    S4: 'S4',
+    S5: 'S5',
+    S6: 'S6',
+    S7: 'S7',
+    S8: 'S8',
+    TT: 'TT'
+  },
+  [Make.SEAT]: {
+    ALHAMBRA: 'Alhambra',
+    ALTEA: 'Altea',
+    ARONA: 'Arona',
+    ATECA: 'Ateca',
+    IBIZA: 'Ibiza',
+    LEON: 'Leon',
+    MII: 'Mii',
+    TOLEDO: 'Toledo'
+  },
+  [Make.SKODA]: {
+    CITIGO: 'Citigo',
+    FABIA: 'Fabia',
+    KAROQ: 'Karoq',
+    KODIAQ: 'Kodiaq',
+    OCTAVIA: 'Octavia',
+    RAPID: 'Rapid',
+    SUPERB: 'Superb'
+  },
+  [Make.VOLKSWAGEN]: {
+    AMAROK: 'Amarok',
+    ARTEON: 'Arteon',
+    BEETLE: 'Beetle',
+    CADDY: 'Caddy',
+    CALIFORNIA: 'California',
+    CARAVELLE: 'Caravelle',
+    CC: 'CC',
+    CRAFTER: 'Crafter',
+    EOS: 'Eos',
+    FOX: 'Fox',
+    GOLF: 'Golf',
+    JETTA: 'Jetta',
+    LUPO: 'Lupo',
+    PASSAT: 'Passat',
+    POLO: 'Polo',
+    MULTIVAN: 'Multivan',
+    SCIROCCO: 'Scirocco',
+    SHARAN: 'Sharan',
+    TROC: 'T-Roc',
+    TIGUAN: 'Tiguan',
+    TOUAREG: 'Touareg',
+    TOURAN: 'Touran',
+    TRANSPORTER: 'Transporter',
+    UP: 'up!'
+  }
+}
+
+const FuelType = {
+  DIESEL: 'Diesel',
+  GASOLINE: 'Gasoline',
+  ELECTRIC: 'Electric',
+  HYBRID: 'Hybrid'
+}
+
+const validateVIN = vin => {
+  const regex = /^[ABCDEFGHJKLMNPRSTUVWXYZ0-9]{9}[ABCDEFGHJKLMNPRSTVWXY1-9]{1}[ABCDEFGHJKLMNPRSTUVWXYZ0-9]{7}$/i
+  return regex.test(vin)
+}
+
+const getMake = car => {
+  if (validateVIN(car.vin)) {
+    switch (car.vin.substr(0, 3)) {
+      case 'AAV':
+      case 'LFV':
+      case 'LSV':
+      case 'MEX':
+      case 'VWV':
+      case 'WVG':
+      case 'WVW':
+      case 'WV1':
+      case 'WV2':
+      case 'WV3':
+      case 'XW8':
+      case 'YBW':
+      case '1VW':
+      case '2V4':
+      case '2V8':
+      case '3VW':
+      case '8AW':
+      case '9BW':
+        return Make.VOLKSWAGEN
+      case 'TMB':
+      case 'TMP':
+      case 'TM9':
+        return Make.SKODA
+      case 'TRU':
+      case 'WAU':
+      case 'WA1':
+      case 'WUA':
+      case '93U':
+      case '93V':
+      case '99A':
+        return Make.AUDI
+      case 'VSS':
+        return Make.SEAT
+    }
+  }
+  if (car.name) {
+    if (car.name.match(/audi/i)) {
+      return Make.AUDI
+    }
+    if (car.name.match(/seat/i)) {
+      return Make.SEAT
+    }
+    if (car.name.match(/(škoda|skoda)/i)) {
+      return Make.SKODA
+    }
+    if (car.name.match(/(volkswagen|vw)/i)) {
+      return Make.VOLKSWAGEN
+    }
+  }
+  return null
+}
+
+const getModel = car => {
+  if (car.name) {
+    if (car.name.match(/(audi a1|(^a1( |$)))/i)) {
+      return Model[Make.AUDI].A1
+    }
+    if (car.name.match(/(audi a3|(^a3( |$)))/i)) {
+      return Model[Make.AUDI].A3
+    }
+    if (car.name.match(/(audi a4|(^a4( |$)))/i)) {
+      return Model[Make.AUDI].A4
+    }
+    if (car.name.match(/(audi a5|(^a5( |$)))/i)) {
+      return Model[Make.AUDI].A5
+    }
+    if (car.name.match(/(audi a6|(^a6( |$)))/i)) {
+      return Model[Make.AUDI].A6
+    }
+    if (car.name.match(/(audi a7|(^a7( |$)))/i)) {
+      return Model[Make.AUDI].A7
+    }
+    if (car.name.match(/(audi a8|(^a8( |$)))/i)) {
+      return Model[Make.AUDI].A8
+    }
+    if (car.name.match(/(audi q2|(^q2( |$)))/i)) {
+      return Model[Make.AUDI].Q2
+    }
+    if (car.name.match(/(audi q3|(^q3( |$)))/i)) {
+      return Model[Make.AUDI].Q3
+    }
+    if (car.name.match(/(audi q5|(^q5( |$)))/i)) {
+      return Model[Make.AUDI].Q5
+    }
+    if (car.name.match(/(audi q7|(^q7( |$)))/i)) {
+      return Model[Make.AUDI].Q7
+    }
+    if (car.name.match(/(audi r8|(^r8( |$)))/i)) {
+      return Model[Make.AUDI].R8
+    }
+    if (car.name.match(/(audi rs3|(^rs3( |$)))/i)) {
+      return Model[Make.AUDI].RS3
+    }
+    if (car.name.match(/(audi rs4|(^rs4( |$)))/i)) {
+      return Model[Make.AUDI].RS4
+    }
+    if (car.name.match(/(audi rs5|(^rs5( |$)))/i)) {
+      return Model[Make.AUDI].RS5
+    }
+    if (car.name.match(/(audi rs6|(^rs6( |$)))/i)) {
+      return Model[Make.AUDI].RS6
+    }
+    if (car.name.match(/(audi rs7|(^rs7( |$)))/i)) {
+      return Model[Make.AUDI].RS7
+    }
+    if (car.name.match(/(audi s1|(^s1( |$)))/i)) {
+      return Model[Make.AUDI].S1
+    }
+    if (car.name.match(/(audi s3|(^s3( |$)))/i)) {
+      return Model[Make.AUDI].S3
+    }
+    if (car.name.match(/(audi s4|(^s4( |$)))/i)) {
+      return Model[Make.AUDI].S4
+    }
+    if (car.name.match(/(audi s5|(^s5( |$)))/i)) {
+      return Model[Make.AUDI].S5
+    }
+    if (car.name.match(/(audi s6|(^s6( |$)))/i)) {
+      return Model[Make.AUDI].S6
+    }
+    if (car.name.match(/(audi s7|(^s7( |$)))/i)) {
+      return Model[Make.AUDI].S7
+    }
+    if (car.name.match(/(audi s8|(^s8( |$)))/i)) {
+      return Model[Make.AUDI].S8
+    }
+    if (car.name.match(/(audi tt|(^tt( |$)))/i)) {
+      return Model[Make.AUDI].TT
+    }
+    if (car.name.match(/alhambra/i)) {
+      return Model[Make.SEAT].ALHAMBRA
+    }
+    if (car.name.match(/altea/i)) {
+      return Model[Make.SEAT].ALTEA
+    }
+    if (car.name.match(/arona/i)) {
+      return Model[Make.SEAT].ARONA
+    }
+    if (car.name.match(/ateca/i)) {
+      return Model[Make.SEAT].ATECA
+    }
+    if (car.name.match(/ibiza/i)) {
+      return Model[Make.SEAT].IBIZA
+    }
+    if (car.name.match(/leon/i)) {
+      return Model[Make.SEAT].LEON
+    }
+    if (car.name.match(/mii/i)) {
+      return Model[Make.SEAT].MII
+    }
+    if (car.name.match(/toledo/i)) {
+      return Model[Make.SEAT].TOLEDO
+    }
+    if (car.name.match(/citigo/i)) {
+      return Model[Make.SKODA].CITIGO
+    }
+    if (car.name.match(/fabia/i)) {
+      return Model[Make.SKODA].FABIA
+    }
+    if (car.name.match(/karoq/i)) {
+      return Model[Make.SKODA].KAROQ
+    }
+    if (car.name.match(/kodiaq/i)) {
+      return Model[Make.SKODA].KODIAQ
+    }
+    if (car.name.match(/octavia/i)) {
+      return Model[Make.SKODA].OCTAVIA
+    }
+    if (car.name.match(/rapid/i)) {
+      return Model[Make.SKODA].RAPID
+    }
+    if (car.name.match(/superb/i)) {
+      return Model[Make.SKODA].SUPERB
+    }
+    if (car.name.match(/amarok/i)) {
+      return Model[Make.VOLKSWAGEN].AMAROK
+    }
+    if (car.name.match(/arteon/i)) {
+      return Model[Make.VOLKSWAGEN].ARTEON
+    }
+    if (car.name.match(/beetle/i)) {
+      return Model[Make.VOLKSWAGEN].BEETLE
+    }
+    if (car.name.match(/caddy/i)) {
+      return Model[Make.VOLKSWAGEN].CADDY
+    }
+    if (car.name.match(/california/i)) {
+      return Model[Make.VOLKSWAGEN].CALIFORNIA
+    }
+    if (car.name.match(/caravelle/i)) {
+      return Model[Make.VOLKSWAGEN].CARAVELLE
+    }
+    if (car.name.match(/( |^)cc( |$)/i)) {
+      return Model[Make.VOLKSWAGEN].CC
+    }
+    if (car.name.match(/crafter/i)) {
+      return Model[Make.VOLKSWAGEN].CRAFTER
+    }
+    if (car.name.match(/eos/i)) {
+      return Model[Make.VOLKSWAGEN].EOS
+    }
+    if (car.name.match(/fox/i)) {
+      return Model[Make.VOLKSWAGEN].FOX
+    }
+    if (car.name.match(/golf/i)) {
+      return Model[Make.VOLKSWAGEN].GOLF
+    }
+    if (car.name.match(/jetta/i)) {
+      return Model[Make.VOLKSWAGEN].JETTA
+    }
+    if (car.name.match(/lupo/i)) {
+      return Model[Make.VOLKSWAGEN].LUPO
+    }
+    if (car.name.match(/multivan/i)) {
+      return Model[Make.VOLKSWAGEN].MULTIVAN
+    }
+    if (car.name.match(/passat/i)) {
+      return Model[Make.VOLKSWAGEN].PASSAT
+    }
+    if (car.name.match(/polo/i)) {
+      return Model[Make.VOLKSWAGEN].POLO
+    }
+    if (car.name.match(/scirocco/i)) {
+      return Model[Make.VOLKSWAGEN].SCIROCCO
+    }
+    if (car.name.match(/sharan/i)) {
+      return Model[Make.VOLKSWAGEN].SHARAN
+    }
+    if (car.name.match(/(troc|t-roc|t roc)/i)) {
+      return Model[Make.VOLKSWAGEN].TROC
+    }
+    if (car.name.match(/tiguan/i)) {
+      return Model[Make.VOLKSWAGEN].TIGUAN
+    }
+    if (car.name.match(/touareg/i)) {
+      return Model[Make.VOLKSWAGEN].TOUAREG
+    }
+    if (car.name.match(/touran/i)) {
+      return Model[Make.VOLKSWAGEN].TOURAN
+    }
+    if (car.name.match(/(kassevogn|transporter)/i)) {
+      return Model[Make.VOLKSWAGEN].TRANSPORTER
+    }
+    if (car.name.match(/(up!|up( |$))/i)) {
+      return Model[Make.VOLKSWAGEN].UP
+    }
+  }
+  return null
+}
+
+/**
+ * This method retrieves the engine size for a car from the vehicle description 
+ * (name).
+ * 
+ * @param {{ name: string }} car
+ * @returns {string|null} Formatted as `1.4`
+ */
+const getEngineSize = car => {
+  if (car.name) {
+    const match = car.name.match(/(?:[^\d]|^)(\d[,.]\d)(?:[^\d]|$)/)
+    if (match) {
+      return match[1].replace(',', '.')
+    }
+  }
+  return null
+}
+
+/**
+ * This method retrieves the fuel type for a car from the vehicle description 
+ * (name) or by some predefined rules for specific models.
+ * 
+ * @param {{ vin: string, name: string }} car
+ * @returns {'Gasoline'|'Diesel'|null}
+ */
+const getFuelType = car => {
+  if (car.name) {
+    if (car.name.match(/(e-golf|e-up!|e-up( |$))/i)) {
+      return FuelType.ELECTRIC
+    }
+    if (car.name.match(/(gte|e-tron)/i)) {
+      return FuelType.HYBRID
+    }
+    if (car.name.match(/(sdi|tdi)/i)) {
+      return FuelType.DIESEL
+    }
+    if (car.name.match(/(fsi|mpi|tfsi|tsi)/i)) {
+      return FuelType.GASOLINE
+    }
+  }
+  const model = getModel(car)
+  if (
+    model === Model[Make.SEAT].MII ||
+    model === Model[Make.SKODA].CITIGO ||
+    model === Model[Make.VOLKSWAGEN].UP
+  ) {
+    return FuelType.GASOLINE
+  }
+  return null
+}
+
+/**
+ * This method retrieves the year for a car from the VIN.
+ * Year from VIN uses the 10th character according to the specification, but 
+ * resolves ambiguity to the range 2000-2029.
+ * 
+ * @param {{ vin: string }} car
+ * @returns {integer|null} Formatted with four digits
+ */
+const getYear = car => {
+  if (validateVIN(car.vin)) {
+    switch (car.vin.substr(9, 1).toUpperCase()) {
+      case 'Y':
+        return 2000
+      case '1':
+        return 2001
+      case '2':
+        return 2002
+      case '3':
+        return 2003
+      case '4':
+        return 2004
+      case '5':
+        return 2005
+      case '6':
+        return 2006
+      case '7':
+        return 2007
+      case '8':
+        return 2008
+      case '9':
+        return 2009
+      case 'A':
+        return 2010
+      case 'B':
+        return 2011
+      case 'C':
+        return 2012
+      case 'D':
+        return 2013
+      case 'E':
+        return 2014
+      case 'F':
+        return 2015
+      case 'G':
+        return 2016
+      case 'H':
+        return 2017
+      case 'J':
+        return 2018
+      case 'K':
+        return 2019
+      case 'L':
+        return 2020
+      case 'M':
+        return 2021
+      case 'N':
+        return 2022
+      case 'P':
+        return 2023
+      case 'R':
+        return 2024
+      case 'S':
+        return 2025
+      case 'T':
+        return 2026
+      case 'V':
+        return 2027
+      case 'W':
+        return 2028
+      case 'X':
+        return 2029
+    }
+  }
+  return null
+}
+
+module.exports = {
+  Make,
+  Model,
+  FuelType,
+  getMake,
+  getModel,
+  getEngineSize,
+  getFuelType,
+  getYear
+}
---
+++ src/index.test.js
@@ -0,0 +1,271 @@
+const expect = require('unexpected')
+const {
+  getMake,
+  getModel,
+  getEngineSize,
+  getFuelType,
+  getYear
+} = require('.')
+
+describe('utils/car', () => {
+  it('runs getMake', () => {
+    expect(getMake({ vin: 'AAVZZZAAZFD123456' }), 'to be', 'Volkswagen')
+    expect(getMake({ vin: 'LFVZZZAAZFD123456' }), 'to be', 'Volkswagen')
+    expect(getMake({ vin: 'LSVZZZAAZFD123456' }), 'to be', 'Volkswagen')
+    expect(getMake({ vin: 'MEXZZZAAZFD123456' }), 'to be', 'Volkswagen')
+    expect(getMake({ vin: 'VWVZZZAAZFD123456' }), 'to be', 'Volkswagen')
+    expect(getMake({ vin: 'WVGZZZAAZFD123456' }), 'to be', 'Volkswagen')
+    expect(getMake({ vin: 'WVWZZZAAZFD123456' }), 'to be', 'Volkswagen')
+    expect(getMake({ vin: 'WV1ZZZAAZFD123456' }), 'to be', 'Volkswagen')
+    expect(getMake({ vin: 'WV2ZZZAAZFD123456' }), 'to be', 'Volkswagen')
+    expect(getMake({ vin: 'WV3ZZZAAZFD123456' }), 'to be', 'Volkswagen')
+    expect(getMake({ vin: 'XW8ZZZAAZFD123456' }), 'to be', 'Volkswagen')
+    expect(getMake({ vin: 'YBWZZZAAZFD123456' }), 'to be', 'Volkswagen')
+    expect(getMake({ vin: '1VWZZZAAZFD123456' }), 'to be', 'Volkswagen')
+    expect(getMake({ vin: '2V4ZZZAAZFD123456' }), 'to be', 'Volkswagen')
+    expect(getMake({ vin: '2V8ZZZAAZFD123456' }), 'to be', 'Volkswagen')
+    expect(getMake({ vin: '3VWZZZAAZFD123456' }), 'to be', 'Volkswagen')
+    expect(getMake({ vin: '8AWZZZAAZFD123456' }), 'to be', 'Volkswagen')
+    expect(getMake({ vin: '9BWZZZAAZFD123456' }), 'to be', 'Volkswagen')
+    expect(getMake({ vin: 'TMBJE73T3E9123456' }), 'to be', 'Škoda')
+    expect(getMake({ vin: 'TMPJE73T3E9123456' }), 'to be', 'Škoda')
+    expect(getMake({ vin: 'TM9JE73T3E9123456' }), 'to be', 'Škoda')
+    expect(getMake({ vin: 'TRUZZZ8V1H1123456' }), 'to be', 'Audi')
+    expect(getMake({ vin: 'WAUZZZ8V1H1123456' }), 'to be', 'Audi')
+    expect(getMake({ vin: 'WA1ZZZ8V1H1123456' }), 'to be', 'Audi')
+    expect(getMake({ vin: 'WUAZZZ4F3BN123456' }), 'to be', 'Audi')
+    expect(getMake({ vin: '93UZZZ8V1H1123456' }), 'to be', 'Audi')
+    expect(getMake({ vin: '93VZZZ8V1H1123456' }), 'to be', 'Audi')
+    expect(getMake({ vin: '99AZZZ8V1H1123456' }), 'to be', 'Audi')
+    expect(getMake({ vin: 'VSSZZZ5FZGR123456' }), 'to be', 'SEAT')
+    expect(getMake({ name: 'volkswagen' }), 'to be', 'Volkswagen')
+    expect(getMake({ name: 'VOLKSWAGEN' }), 'to be', 'Volkswagen')
+    expect(getMake({ name: 'Volkswagen' }), 'to be', 'Volkswagen')
+    expect(getMake({ name: ' volkswagen something' }), 'to be', 'Volkswagen')
+    expect(getMake({ name: 'vw' }), 'to be', 'Volkswagen')
+    expect(getMake({ name: 'VW' }), 'to be', 'Volkswagen')
+    expect(getMake({ name: 'Vw' }), 'to be', 'Volkswagen')
+    expect(getMake({ name: ' vw something' }), 'to be', 'Volkswagen')
+    expect(getMake({ name: 'škoda' }), 'to be', 'Škoda')
+    expect(getMake({ name: 'ŠKODA' }), 'to be', 'Škoda')
+    expect(getMake({ name: 'Škoda' }), 'to be', 'Škoda')
+    expect(getMake({ name: ' škoda something' }), 'to be', 'Škoda')
+    expect(getMake({ name: 'skoda' }), 'to be', 'Škoda')
+    expect(getMake({ name: 'SKODA' }), 'to be', 'Škoda')
+    expect(getMake({ name: 'Skoda' }), 'to be', 'Škoda')
+    expect(getMake({ name: ' skoda something' }), 'to be', 'Škoda')
+    expect(getMake({ name: 'audi' }), 'to be', 'Audi')
+    expect(getMake({ name: 'AUDI' }), 'to be', 'Audi')
+    expect(getMake({ name: 'Audi' }), 'to be', 'Audi')
+    expect(getMake({ name: ' audi something' }), 'to be', 'Audi')
+    expect(getMake({ name: 'seat' }), 'to be', 'SEAT')
+    expect(getMake({ name: 'SEAT' }), 'to be', 'SEAT')
+    expect(getMake({ name: 'Seat' }), 'to be', 'SEAT')
+    expect(getMake({ name: ' seat something' }), 'to be', 'SEAT')
+    expect(
+      getMake({ vin: 'WVWZZZAAZFD123456', name: 'Audi' }),
+      'to be',
+      'Volkswagen'
+    )
+    expect(
+      getMake({ vin: 'TMBJE73T3E9123456', name: 'Audi' }),
+      'to be',
+      'Škoda'
+    )
+    expect(
+      getMake({ vin: 'WAUZZZ8V1H1123456', name: 'Volkswagen' }),
+      'to be',
+      'Audi'
+    )
+    expect(getMake({ vin: 'VSSZZZ5FZGR123456', name: 'Audi' }), 'to be', 'SEAT')
+    expect(getMake({ vin: 'VSSbogus', name: 'Audi' }), 'to be', 'Audi')
+    expect(getMake({ vin: 'AAAUNKNOWNMAKEAAA' }), 'to be null')
+    expect(getMake({ vin: 'VSS-invalid-vin' }), 'to be null')
+    expect(getMake({ name: 'bogus' }), 'to be null')
+    expect(getMake({}), 'to be null')
+  })
+
+  it('runs getModel', () => {
+    expect(
+      getModel({ name: 'VW Touran CL 1,4TSI 150HK DSG7 BMT 110kW' }),
+      'to be',
+      'Touran'
+    )
+    expect(getModel({ name: 'MOVE UP! 1,0MPI 60HK 5G BMT ' }), 'to be', 'up!')
+    expect(
+      getModel({ name: 'SKODA Octavia HB Style 1,4 TSI 150 HK DSG' }),
+      'to be',
+      'Octavia'
+    )
+    expect(
+      getModel({ name: 'VW PASSAT VAR COMF. 1,6TDI 105HK 6G' }),
+      'to be',
+      'Passat'
+    )
+    expect(
+      getModel({ name: 'VW POLO COMF 1,6TDI 90HK 5G BMT 66KW' }),
+      'to be',
+      'Polo'
+    )
+    expect(getModel({ name: 'VW move up 1,0MPI 60HK 5G BMT' }), 'to be', 'up!')
+    expect(
+      getModel({ name: 'TOLEDO STYLE 1,2 TSI 105HK 6 G' }),
+      'to be',
+      'Toledo'
+    )
+    expect(
+      getModel({ name: 'SKODA Superb Combi Style 2,0 TDI 150 hk DSG' }),
+      'to be',
+      'Superb'
+    )
+    expect(getModel({ name: 'Tiguan' }), 'to be', 'Tiguan')
+    expect(
+      getModel({ name: 'VW Golf Var Allstar 1,4TSI 125HK 6G BMT' }),
+      'to be',
+      'Golf'
+    )
+    expect(getModel({ name: 'Golf ' }), 'to be', 'Golf')
+    expect(
+      getModel({ name: 'VW Sharan HL 2,0TDI 184hk DSG6 BMT SCR 135k' }),
+      'to be',
+      'Sharan'
+    )
+    expect(getModel({ name: 'a4' }), 'to be', 'A4')
+    expect(getModel({ name: 'NotA4' }), 'to be null')
+    expect(getModel({ name: 'Audi A3' }), 'to be', 'A3')
+    expect(getModel({ name: 'up!' }), 'to be', 'up!')
+    expect(getModel({ name: 'Volkswagen CC' }), 'to be', 'CC')
+    expect(getModel({ name: 'Bogus CCS' }), 'to be null')
+    expect(getModel({ name: 'bogus' }), 'to be null')
+    expect(getModel({}), 'to be null')
+  })
+
+  it('runs getEngineSize', () => {
+    expect(getEngineSize({ name: '1.0' }), 'to be', '1.0')
+    expect(getEngineSize({ name: '1,2' }), 'to be', '1.2')
+    expect(getEngineSize({ name: '1.4TSI' }), 'to be', '1.4')
+    expect(getEngineSize({ name: 'Golf 1.6 TDI' }), 'to be', '1.6')
+    expect(getEngineSize({ name: 'Audi A4 2.0 TFSI' }), 'to be', '2.0')
+    expect(getEngineSize({ name: 'a6.5b' }), 'to be', '6.5')
+    expect(getEngineSize({ name: '3.00' }), 'to be null')
+    expect(getEngineSize({ name: '10.1' }), 'to be null')
+    expect(getEngineSize({ name: 'bogus' }), 'to be null')
+    expect(getEngineSize({}), 'to be null')
+  })
+
+  it('runs getFuelType', () => {
+    expect(
+      getFuelType({ name: 'Passat Var HL 2,0TSI 280HK DSG6 4M BMT' }),
+      'to be',
+      'Gasoline'
+    )
+    expect(
+      getFuelType({ name: 'Tiguan CL 2,0TDI 150HK DSG7 BMT SCR' }),
+      'to be',
+      'Diesel'
+    )
+    expect(
+      getFuelType({ name: 'tiguan cl 2,0tdi 150hk dsg7 bmt scr' }),
+      'to be',
+      'Diesel'
+    )
+    expect(
+      getFuelType({ name: 'Audi A3 Cab Sport 1,4 TFSI 150 HK' }),
+      'to be',
+      'Gasoline'
+    )
+    expect(
+      getFuelType({ name: 'high up! 1,0MPI 75HK 5G BMT 55kW' }),
+      'to be',
+      'Gasoline'
+    )
+    expect(
+      getFuelType({ name: 'HIGH UP 1,0MPI 75HK 5G BMT 55kW' }),
+      'to be',
+      'Gasoline'
+    )
+    expect(
+      getFuelType({ name: 'VW move up! 1,0 60HK 5G' }),
+      'to be',
+      'Gasoline'
+    )
+    expect(
+      getFuelType({ name: 'VW PASSAT L COMF 2,0L FSI 150 HK 110KW 6G' }),
+      'to be',
+      'Gasoline'
+    )
+    expect(
+      getFuelType({ name: 'VW Golf Limo GTE 1,4TSI 204HK Hybrid DSG6' }),
+      'to be',
+      'Hybrid'
+    )
+    expect(
+      getFuelType({ name: 'Audi A3 e-tron Sport 1,4 TFSI 150 HK' }),
+      'to be',
+      'Hybrid'
+    )
+    expect(
+      getFuelType({ name: 'VW e-Golf 115HK 85kW 1AUT' }),
+      'to be',
+      'Electric'
+    )
+    expect(
+      getFuelType({ name: 'VW E-UP! 82HK AUT1 60KW' }),
+      'to be',
+      'Electric'
+    )
+    expect(getFuelType({ name: 'vw e-up 82hk aut1 60kw' }), 'to be', 'Electric')
+    expect(getFuelType({ name: 'e-golf' }), 'to be', 'Electric')
+    expect(getFuelType({ name: 'mii' }), 'to be', 'Gasoline')
+    expect(getFuelType({ name: 'citigo' }), 'to be', 'Gasoline')
+    expect(getFuelType({ name: 'up!' }), 'to be', 'Gasoline')
+    expect(getFuelType({ name: 'e-upper' }), 'to be null')
+    expect(getFuelType({ name: 'bogus' }), 'to be null')
+    expect(getFuelType({}), 'to be null')
+  })
+
+  it('runs getYear', () => {
+    // ABCDEFGHJKLMNPRSTVWXY1-9
+    expect(getYear({ vin: 'WVWZZZAAZAD123456' }), 'to be', 2010)
+    expect(getYear({ vin: 'WVWZZZAAZBD123456' }), 'to be', 2011)
+    expect(getYear({ vin: 'WVWZZZAAZCD123456' }), 'to be', 2012)
+    expect(getYear({ vin: 'WVWZZZAAZDD123456' }), 'to be', 2013)
+    expect(getYear({ vin: 'WVWZZZAAZED123456' }), 'to be', 2014)
+    expect(getYear({ vin: 'WVWZZZAAZFD123456' }), 'to be', 2015)
+    expect(getYear({ vin: 'WVWZZZAAZGD123456' }), 'to be', 2016)
+    expect(getYear({ vin: 'WVWZZZAAZHD123456' }), 'to be', 2017)
+    expect(getYear({ vin: 'WVWZZZAAZID123456' }), 'to be null')
+    expect(getYear({ vin: 'WVWZZZAAZJD123456' }), 'to be', 2018)
+    expect(getYear({ vin: 'WVWZZZAAZKD123456' }), 'to be', 2019)
+    expect(getYear({ vin: 'WVWZZZAAZLD123456' }), 'to be', 2020)
+    expect(getYear({ vin: 'WVWZZZAAZMD123456' }), 'to be', 2021)
+    expect(getYear({ vin: 'WVWZZZAAZND123456' }), 'to be', 2022)
+    expect(getYear({ vin: 'WVWZZZAAZOD123456' }), 'to be null')
+    expect(getYear({ vin: 'WVWZZZAAZPD123456' }), 'to be', 2023)
+    expect(getYear({ vin: 'WVWZZZAAZQD123456' }), 'to be null')
+    expect(getYear({ vin: 'WVWZZZAAZRD123456' }), 'to be', 2024)
+    expect(getYear({ vin: 'WVWZZZAAZSD123456' }), 'to be', 2025)
+    expect(getYear({ vin: 'WVWZZZAAZTD123456' }), 'to be', 2026)
+    expect(getYear({ vin: 'WVWZZZAAZUD123456' }), 'to be null')
+    expect(getYear({ vin: 'WVWZZZAAZVD123456' }), 'to be', 2027)
+    expect(getYear({ vin: 'WVWZZZAAZWD123456' }), 'to be', 2028)
+    expect(getYear({ vin: 'WVWZZZAAZXD123456' }), 'to be', 2029)
+    expect(getYear({ vin: 'WVWZZZAAZYD123456' }), 'to be', 2000)
+    expect(getYear({ vin: 'WVWZZZAAZZD123456' }), 'to be null')
+    expect(getYear({ vin: 'WVWZZZAAZ0D123456' }), 'to be null')
+    expect(getYear({ vin: 'WVWZZZAAZ1D123456' }), 'to be', 2001)
+    expect(getYear({ vin: 'WVWZZZAAZ2D123456' }), 'to be', 2002)
+    expect(getYear({ vin: 'WVWZZZAAZ3D123456' }), 'to be', 2003)
+    expect(getYear({ vin: 'WVWZZZAAZ4D123456' }), 'to be', 2004)
+    expect(getYear({ vin: 'WVWZZZAAZ5D123456' }), 'to be', 2005)
+    expect(getYear({ vin: 'WVWZZZAAZ6D123456' }), 'to be', 2006)
+    expect(getYear({ vin: 'WVWZZZAAZ7D123456' }), 'to be', 2007)
+    expect(getYear({ vin: 'WVWZZZAAZ8D123456' }), 'to be', 2008)
+    expect(getYear({ vin: 'WVWZZZAAZ9D123456' }), 'to be', 2009)
+    expect(getYear({ vin: 'AAAAAAAAAaAAAAAAA' }), 'to be', 2010)
+    expect(getYear({ vin: 'AAAAAAAAA0AAAAAAA' }), 'to be null')
+    expect(getYear({ vin: 'VSS-invalid-vin' }), 'to be null')
+    expect(getYear({ vin: 'bogus' }), 'to be null')
+    expect(getYear({}), 'to be null')
+  })
+})
