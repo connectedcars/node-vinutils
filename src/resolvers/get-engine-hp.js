a1ccd469d18d02b497280e6850188adf096f42d1
Christian Danielsen <christiandefries@gmail.com>
2019-12-04 09:25:49 +0100

    Add TRP to get-model, add new function to get horse power (#13)
    * Add TRP to get-model, add new function to get horse power
    
    * Fix picking wrong match group for hp
    
    Also adds tests
    
    * Fix typo

---
+++ src/resolvers/get-engine-hp.js
@@ -0,0 +1,19 @@
+const getEngineHorsepowerFromDescription = description => {
+  const match = description.match(/(\d{2}|\d{3})(| )hk|(\d{2}|\d{3})(| )hp/)
+  if (match) {
+    return parseInt(match[1] || match[3])
+  }
+  return null
+}
+
+/**
+ * Returns the engine horsepower given the description
+ * @param {string} description description formatted as `102hk,80hk,102hp,80hp`
+ */
+module.exports = description => {
+  let engineSize = null
+  if (description) {
+    engineSize = getEngineHorsepowerFromDescription(description.toLowerCase())
+  }
+  return engineSize
+}
