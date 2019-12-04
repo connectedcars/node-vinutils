18cd5bc3be1f8e02dba673e5d7fe5bbe14064cec
chribsen <cd@connectedcars.dk>
2019-12-04 15:56:32 +0100

    Create a literal string union to make typing more strict

--- src/types/@connectedcars/vinutils/index.d.ts
+++ src/types/@connectedcars/vinutils/index.d.ts
@@ -1,154 +1,226 @@
 export const FuelType: {
-  DIESEL: string
-  ELECTRIC: string
-  GASOLINE: string
-  HYBRID: string
+  DIESEL: 'Diesel'
+  GASOLINE: 'Gasoline'
+  ELECTRIC: 'Electric'
+  HYBRID: 'Hybrid'
 }
-export const Make: {
-  ALFA_ROMEO: string
-  ASTON_MARTIN: string
-  AUDI: string
-  BENTLEY: string
-  BMW: string
-  BUGATTI: string
-  BUICK: string
-  CADILLAC: string
-  CHEVROLET: string
-  CHRYSLER: string
-  CITROEN: string
-  DACIA: string
-  DAEWOO: string
-  DAF: string
-  DAIHATSU: string
-  DELOREAN: string
-  DODGE: string
-  DUCATI: string
-  FERRARI: string
-  FIAT: string
-  FORD: string
-  FREIGHTLINER: string
-  GENERAL_MOTORS: string
-  HARLEY_DAVIDSON: string
-  HONDA: string
-  HYUNDAI: string
-  IKARUS: string
-  ISUZU: string
-  IVECO: string
-  JAGUAR: string
-  JEEP: string
-  KAWASAKI: string
-  KENWORTH: string
-  KIA: string
-  KOENIGSEGG: string
-  LADA: string
-  LAMBORGHINI: string
-  LANCIA: string
-  LAND_ROVER: string
-  LINCOLN: string
-  LOTUS: string
-  MACK: string
-  MAN: string
-  MASERATI: string
-  MAZDA: string
-  MCLAREN: string
-  MERCEDES_BENZ: string
-  MERCURY: string
-  MG: string
-  MINI: string
-  MITSUBISHI: string
-  NISSAN: string
-  NOVA: string
-  OLDSMOBILE: string
-  OPEL: string
-  PEUGEOT: string
-  PLYMOUTH: string
-  PONTIAC: string
-  PORSCHE: string
-  RENAULT: string
-  ROLLS_ROYCE: string
-  SAAB: string
-  SATURN: string
-  SCANIA: string
-  SEAT: string
-  SKODA: string
-  SMART: string
-  SOLARIS: string
-  SSANGYONG: string
-  SUBARU: string
-  SUZUKI: string
-  TESLA: string
-  TOYOTA: string
-  TRIUMPH: string
-  VOLKSWAGEN: string
-  VOLVO: string
-  WESTERN_STAR: string
-  YAMAHA: string
+export type Make = {
+  ALFA_ROMEO: 'Alfa Romeo'
+  ASTON_MARTIN: 'Aston Martin'
+  AUDI: 'Audi'
+  BMW: 'BMW'
+  BENTLEY: 'Bentley'
+  BUGATTI: 'Bugatti'
+  BUICK: 'Buick'
+  CADILLAC: 'Cadillac'
+  CHEVROLET: 'Chevrolet'
+  CHRYSLER: 'Chrysler'
+  CITROEN: 'Citroën'
+  DACIA: 'Dacia'
+  DAF: 'DAF'
+  DAEWOO: 'Daewoo'
+  DAIHATSU: 'Daihatsu'
+  DELOREAN: 'DeLorean'
+  DODGE: 'Dodge'
+  DUCATI: 'Ducati'
+  FERRARI: 'Ferrari'
+  FIAT: 'Fiat'
+  FORD: 'Ford'
+  FREIGHTLINER: 'Freightliner'
+  GENERAL_MOTORS: 'General Motors'
+  HARLEY_DAVIDSON: 'Harley Davidson'
+  HONDA: 'Honda'
+  HYUNDAI: 'Hyundai'
+  IKARUS: 'Ikarus'
+  ISUZU: 'Isuzu'
+  IVECO: 'Iveco'
+  JAGUAR: 'Jaguar'
+  JEEP: 'Jeep'
+  KAWASAKI: 'Kawasaki'
+  KENWORTH: 'Kenworth'
+  KIA: 'Kia'
+  KOENIGSEGG: 'Koenigsegg'
+  LADA: 'Lada'
+  LAMBORGHINI: 'Lamborghini'
+  LANCIA: 'Lancia'
+  LAND_ROVER: 'Land Rover'
+  LINCOLN: 'Lincoln'
+  LOTUS: 'Lotus'
+  MAN: 'MAN'
+  MG: 'MG'
+  MINI: 'MINI'
+  MACK: 'Mack'
+  MASERATI: 'Maserati'
+  MAZDA: 'Mazda'
+  MCLAREN: 'McLaren'
+  MERCEDES_BENZ: 'Mercedes-Benz'
+  MERCURY: 'Mercury'
+  MITSUBISHI: 'Mitsubishi'
+  NISSAN: 'Nissan'
+  NOVA: 'Nova'
+  OLDSMOBILE: 'Oldsmobile'
+  OPEL: 'Opel'
+  PEUGEOT: 'Peugeot'
+  PLYMOUTH: 'Plymouth'
+  PONTIAC: 'Pontiac'
+  PORSCHE: 'Porsche'
+  RENAULT: 'Renault'
+  ROLLS_ROYCE: 'Rolls Royce'
+  SAAB: 'Saab'
+  SATURN: 'Saturn'
+  SCANIA: 'Scania'
+  SEAT: 'SEAT'
+  SKODA: 'Škoda'
+  SMART: 'smart'
+  SOLARIS: 'Solaris'
+  SSANGYONG: 'SsangYong'
+  SUBARU: 'Subaru'
+  SUZUKI: 'Suzuki'
+  TESLA: 'Tesla'
+  TOYOTA: 'Toyota'
+  TRIUMPH: 'Triumph'
+  VOLKSWAGEN: 'Volkswagen'
+  VOLVO: 'Volvo'
+  YAMAHA: 'Yamaha'
+  WESTERN_STAR: 'Western Star'
 }
-export const Model: {
-  Audi: {
-    A1: string
-    A3: string
-    A4: string
-    A5: string
-    A6: string
-    A7: string
-    A8: string
-    Q2: string
-    Q3: string
-    Q5: string
-    Q7: string
-    R8: string
-    TT: string
+export type Model = {
+  AUDI: {
+    A1: 'A1'
+    A3: 'A3'
+    A4: 'A4'
+    A5: 'A5'
+    A6: 'A6'
+    A7: 'A7'
+    A8: 'A8'
+    Q2: 'Q2'
+    Q3: 'Q3'
+    Q4: 'Q4'
+    Q5: 'Q5'
+    Q7: 'Q7'
+    Q8: 'Q8'
+    R8: 'R8'
+    TT: 'TT'
+    ETRON: 'e-tron'
+  }
+  CITROEN: {
+    BERLINGO: 'Berlingo'
+    JUMPER: 'Jumper'
+    JUMPY: 'Jumpy'
+  }
+  DACIA: {
+    DOKKER: 'Dokker'
+  }
+  FIAT: {
+    DUCATO: 'Ducato'
+    DOBLO: 'Doblò'
+  }
+  FORD: {
+    C_MAX: 'C-MAX'
+    MONDEO: 'Mondeo'
+    TRANSIT: 'Transit'
+  }
+  IVECO: {
+    DAILY: 'Daily'
+  }
+  MERCEDES_BENZ: {
+    SPRINTER: 'Sprinter'
+    VITO: 'Vito'
+  }
+  NISSAN: {
+    NV400: 'NV400'
+  }
+  OPEL: {
+    VIVARO: 'Vivaro'
+    ZAFIRA: 'Zafira'
+  }
+  PEUGEOT: {
+    EXPERT: 'Expert'
+    PARTNER: 'Partner'
+  }
+  RENAULT: {
+    KANGOO: 'Kangoo'
+    MASTER: 'Master'
   }
   SEAT: {
-    ALHAMBRA: string
-    ALTEA: string
-    ARONA: string
-    ATECA: string
-    CORDOBA: string
-    IBIZA: string
-    LEON: string
-    MII: string
-    TOLEDO: string
+    ALHAMBRA: 'Alhambra'
+    ALTEA: 'Altea'
+    ARONA: 'Arona'
+    ATECA: 'Ateca'
+    CORDOBA: 'Cordoba'
+    IBIZA: 'Ibiza'
+    LEON: 'Leon'
+    MII: 'Mii'
+    TARRACO: 'Tarraco'
+    TOLEDO: 'Toledo'
   }
-  Volkswagen: {
-    AMAROK: string
-    ARTEON: string
-    BEETLE: string
-    CADDY: string
-    CALIFORNIA: string
-    CARAVELLE: string
-    CC: string
-    CRAFTER: string
-    EOS: string
-    FOX: string
-    GOLF: string
-    JETTA: string
-    LUPO: string
-    MULTIVAN: string
-    PASSAT: string
-    POLO: string
-    SCIROCCO: string
-    SHARAN: string
-    TIGUAN: string
-    TOUAREG: string
-    TOURAN: string
-    TRANSPORTER: string
-    TROC: string
-    UP: string
+  SKODA: {
+    CITIGO: 'Citigo'
+    FABIA: 'Fabia'
+    KAMIQ: 'Kamiq'
+    KAROQ: 'Karoq'
+    KODIAQ: 'Kodiaq'
+    OCTAVIA: 'Octavia'
+    RAPID: 'Rapid'
+    ROOMSTER: 'Roomster'
+    SCALA: 'Scala'
+    SUPERB: 'Superb'
+    YETI: 'Yeti'
   }
-  Škoda: {
-    CITIGO: string
-    FABIA: string
-    KAROQ: string
-    KODIAQ: string
-    OCTAVIA: string
-    RAPID: string
-    ROOMSTER: string
-    SUPERB: string
-    YETI: string
+  TOYOTA: {
+    HIACE: 'Hiace'
+  }
+  VOLKSWAGEN: {
+    AMAROK: 'Amarok'
+    ARTEON: 'Arteon'
+    BEETLE: 'Beetle'
+    BORA: 'Bora'
+    CADDY: 'Caddy'
+    CALIFORNIA: 'California'
+    CARAVELLE: 'Caravelle'
+    CC: 'CC'
+    CRAFTER: 'Crafter'
+    EOS: 'Eos'
+    FOX: 'Fox'
+    GOLF: 'Golf'
+    GRAND_CALIFORNIA: 'Grand California'
+    JETTA: 'Jetta'
+    LUPO: 'Lupo'
+    PASSAT: 'Passat'
+    PHAETON: 'Phaeton'
+    POLO: 'Polo'
+    MULTIVAN: 'Multivan'
+    SCIROCCO: 'Scirocco'
+    SHARAN: 'Sharan'
+    TCROSS: 'T-Cross'
+    TROC: 'T-Roc'
+    TIGUAN: 'Tiguan'
+    TOUAREG: 'Touareg'
+    TOURAN: 'Touran'
+    TRANSPORTER: 'Transporter'
+    UP: 'up!'
   }
 }
+
+export type MakeType = Make[keyof Make]
+
+type ValueOf<T> = T[keyof T]
+export type ModelType =
+| ValueOf<Model['AUDI']>
+| ValueOf<Model['CITROEN']>
+| ValueOf<Model['DACIA']>
+| ValueOf<Model['FIAT']>
+| ValueOf<Model['FORD']>
+| ValueOf<Model['IVECO']>
+| ValueOf<Model['MERCEDES_BENZ']>
+| ValueOf<Model['NISSAN']>
+| ValueOf<Model['OPEL']>
+| ValueOf<Model['PEUGEOT']>
+| ValueOf<Model['RENAULT']>
+| ValueOf<Model['SEAT']>
+| ValueOf<Model['SKODA']>
+| ValueOf<Model['TOYOTA']>
+| ValueOf<Model['VOLKSWAGEN']>
 
 export function getEngineSize(vehicle: any): any
 export function getFuelType(vehicle: any): any
