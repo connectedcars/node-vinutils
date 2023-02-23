81fdf86e4261632c7c57a83f00d79d269e50cba8
MisanthropicBit <aab@connectedcars.dk>
2023-02-23 15:05:10 +0100

    Update package.json, jest config, and npmignore (#73)

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   // eslint-disable-next-line @typescript-eslint/no-require-imports
-  ...require('@connectedcars/setup/jest.config'),
+  ...require('./node_modules/@connectedcars/setup/jest.config.js'),
   roots: ['<rootDir>/src']
 }
